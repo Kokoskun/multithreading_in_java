311a1cffdbe032ff471b8c313dd81638752ca946
PATIPAN NAKARIN <Kokoskun@gmail.com>
2017-04-18 08:05:56 +0700

    Dev 18_04_2017_08.00

--- CN331_Threads_5710742114/src/Program2/ProgramThread2.java
+++ CN331_Threads_5710742114/src/Program2/ProgramThread2.java
@@ -1,21 +1,19 @@
 package Program2;
-
 public class ProgramThread2 implements Runnable{
-	private static int MAX_VALUE = 100;
+	private static int MAX_VALUE = 1000;
 	private static int PLAYER_ALL = 16;
 	public static int PLAYER_CM = 0;
 	public static String[] PLAYER_WIN;
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		String PlayerName ="";
 		PLAYER_WIN = new String[PLAYER_ALL];
 		for(int item = 1;item <= PLAYER_ALL;item++){
 			PlayerName = "Thread"+item;
-			ProgramThread2 pt1 = new ProgramThread2();
-			Thread thread = new Thread(pt1,PlayerName);
+			ProgramThread2 pt2 = new ProgramThread2();
+			Thread thread = new Thread(pt2,PlayerName);
 			thread.start();
 		}
 	}
-	@Override
 	public void run() {
 		startGame();
 	}
@@ -25,5 +23,18 @@ public class ProgramThread2 implements Runnable{
 		}
 		PLAYER_WIN[PLAYER_CM]= (String) Thread.currentThread().getName();
 		PLAYER_CM += 1;
+		checkWinner();
+	}
+	public void checkWinner(){
+		if(PLAYER_CM==PLAYER_ALL){
+			int valueRating = 1;
+			System.out.println("*---- The winner of the competition!! ----*");
+			for(int itemPlayer = 0;itemPlayer<PLAYER_ALL;itemPlayer++){
+		 		System.out.println("Winner "+valueRating+" : "+PLAYER_WIN[itemPlayer]);
+		 		valueRating += 1;
+			}
+			System.out.println("*------------ END ------------*");
+			System.exit(0);
+		}
 	}
 }
